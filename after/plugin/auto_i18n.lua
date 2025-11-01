a6246368ba2153e9cdb1b209d5ce5112684a1365
Mathis Laroche <ecomath360@gmail.com>
2025-11-01 12:04:58 -0400

    OLD FIX

--- after/plugin/auto_i18n.lua
+++ after/plugin/auto_i18n.lua
@@ -1,9 +1,9 @@
-local i18n = require "auto_i18n"
-
-vim.api.nvim_create_user_command('I18nRefresh',
-  function() i18n.refresh_keys() end, {}
-)
-
-i18n.refresh_keys()
-
-require('cmp').register_source('i18n', i18n.source.new())
+-- local i18n = require "auto_i18n"
+--
+-- vim.api.nvim_create_user_command('I18nRefresh',
+--   function() i18n.refresh_keys() end, {}
+-- )
+--
+-- i18n.refresh_keys()
+--
+-- require('cmp').register_source('i18n', i18n.source.new())
